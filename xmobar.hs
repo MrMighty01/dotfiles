250147b8457c2180e66e0d1cb28a26655243753b
MrMighty01 <abinavrvsa01@gmail.com>
2023-02-26 18:37:06 +0530

    Jus

--- xmobar.hs
+++ xmobar.hs
@@ -75,7 +75,7 @@ Config {
                                        -- discharging status
                                        , "-o"	, "<left>% (<timeleft>)"
                                        -- AC "on" status
-                                       , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                       , "-O"	, "<fc=green>Charging</fc>"
                                        -- charged status
                                        , "-i"	, "<fc=#006000>Charged</fc>"
                              ] 50
